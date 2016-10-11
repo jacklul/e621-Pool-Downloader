8741ceb09fb82d36320e86ab7004d0e183cf1eaa
Jack'lul <jacklul@jacklul.com>
2016-10-11 17:35:36 +0200

    Add noticed about license

--- dlpool.php
+++ dlpool.php
@@ -3,6 +3,10 @@
  * e621 Pool Downloader
  *
  * Copyright (c) 2016 Jack'lul <https://jacklul.com>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed
+ * with this source code.
  */
 
 define("ROOT", dirname(str_replace("phar://", "", __DIR__)));
