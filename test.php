25fa7303d1d8ca454425bd54f3d7c9e6ee19a77f
Jack'lul <jacklulcat@gmail.com>
2018-02-03 23:49:44 +0100

    Improve test script

--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?php
 
-$argv[1] = 378;
+$argv[1] = 'build/test/';
+
+if (!is_dir($argv[1])) {
+    mkdir($argv[1], 0755, true);
+}
+
+file_put_contents('build/test/.poolinfo', 'ID=378');
+
+$argv[1] = realpath($argv[1]);
+
 require_once("build/dlpool.phar");
